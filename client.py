edfa82f4b1bdaa13a979b58fdb6ddbe0e3fa2c31
A.Solovey <solovey.anton@gmail.com>
2018-07-04 14:43:53 +0300

    Code refactorings

--- client.py
+++ client.py
@@ -71,99 +71,6 @@ class Auth():
 
 class Service:
 
-    def __init__(self):
-
-        self.RESOURCES = {
-
-        #   BASE
-        "api": "/api/v1",
-        #   SERVICE
-        "collections": "/data/collections",
-
-        #   SEARCH
-
-        #   AUTH
-        "check_token": "/oauth/check_token",
-
-        #   STATICTICS
-        "current_search_queries": "/search_requests/current",
-        "current_fulltext_queries": "/search_requests/current/ft",
-        "current_dict_queries": "/search_requests/current/dict",
-        "current_dfp_queries": "/search_requests/current/dfp",
-        
-        #   OTHER
-        "upload": "/upload/",   # + name of collection
-        "consoles_versions": "/services/update",
-        "client_console_version": "/services/update/client_console",
-        "admin_console_version": "/services/update/admin_console",
-
-        }
-
-        self.COLLECTIONS = {
-
-        "smtp": "smtp",
-        "pop3": "pop3",
-        "imap": "imap",
-        "mapi": "mapi",
-        "ftp": "ftp",
-        "httpurls": "httpurls",
-        "httpreq": "httpreq",
-        "mailproc": "mailproc",
-        "printer": "printer",
-        "desktop": "desktop",
-        "clipboard": "clipboard",
-        "screenshots": "screenshots",
-        "browsers": "browsers",
-        "keylogger": "keylogger",
-        "devices": "devices",
-        "sharedfiles": "sharedfiles",
-        "cddvd": "cddvd",
-        "usbfiles": "usbfiles",
-        "cloudfiles": "cloudfiles",
-        "wsindexer": "wsindexer",
-        "webmsg": "webmsg",
-        "conversations": "conversations",  
-
-        }
-
-        self.HTTP_ERRORS = {
-
-        "304": "Not Modified",
-        "400": "Bad request",
-        "401": "Not authorized",
-        "403": "Fordidden",
-        "501": "Not implemented",
-        "503": "Service unavailable", 
-
-        }   #   Just FYI
-
-        self.ST_INT_API_ERRORS = {
-        "5": "NotAuthenticated. There is no header with bearer token or Basic not sucsessful.",
-        "7": "AccessForbidden. There is no appropriate right on your token.",
-        "11": "Invalid upload rule. Database not exits OR rotation group not exists.",
-        "13": "UserTokenInvalid. Token type is not supported.",
-        "14": "UserTokenExpired. Token has been expired.",
-        "15": "UserTokenInvalidSignature. Token digital signature is not valid.",
-        }
-    
-    def error_handler(self, http_error_code, int_error_code):
-        # pass    #   TODO:   make API HTTP error codes and internal error codes handler
-
-        if http_error_code == 401 and int_error_code == 5:
-            print(self.ST_INT_API_ERRORS("5"))
-        elif http_error_code == 401 and int_error_code == 13:
-            print(self.ST_INT_API_ERRORS("13"))
-        elif http_error_code == 401 and int_error_code == 14:
-            return(self.ST_INT_API_ERRORS("14"))
-        elif http_error_code == 401 and int_error_code == 15:
-            return(self.ST_INT_API_ERRORS("15"))
-        elif http_error_code == 403 and int_error_code == 7:
-            return(self.ST_INT_API_ERRORS("7"))
-        #   Otherwise print an error
-        else:
-            if ((http_error_code not in self.HTTP_ERRORS) and (int_error_code not in self.ST_INT_API_ERRORS)):
-                print("Unknown error: ", str(http_error_code), json.loads(int_error_code))
-
     def request(self, st_ip, st_port, token, resource, http_method='GET', http_payload=None, offset=None, limit=None):
         #   Abstract class
         #   TODO:   support offset and limit args
@@ -258,6 +165,96 @@ class Upload():
         request = requests.post(upload_url, headers=headers, data=http_payload.SerializeToString())
 
 class Api:
-    def __init__(self, **settings):
-        self._settings = settings
-        # TODO 
+
+    def __init__(self):
+
+        self.RESOURCES = {
+
+            #   BASE
+            "api": "/api/v1",
+            #   SERVICE
+            "collections": "/data/collections",
+
+            #   SEARCH
+
+            #   AUTH
+            "check_token": "/oauth/check_token",
+
+            #   STATICTICS
+            "current_search_queries": "/search_requests/current",
+            "current_fulltext_queries": "/search_requests/current/ft",
+            "current_dict_queries": "/search_requests/current/dict",
+            "current_dfp_queries": "/search_requests/current/dfp",
+            
+            #   OTHER
+            "upload": "/upload/",   # + name of collection
+            "consoles_versions": "/services/update",
+            "client_console_version": "/services/update/client_console",
+            "admin_console_version": "/services/update/admin_console",
+
+        }
+
+        self.COLLECTIONS = {
+
+            "smtp": "smtp",
+            "pop3": "pop3",
+            "imap": "imap",
+            "mapi": "mapi",
+            "ftp": "ftp",
+            "httpurls": "httpurls",
+            "httpreq": "httpreq",
+            "mailproc": "mailproc",
+            "printer": "printer",
+            "desktop": "desktop",
+            "clipboard": "clipboard",
+            "screenshots": "screenshots",
+            "browsers": "browsers",
+            "keylogger": "keylogger",
+            "devices": "devices",
+            "sharedfiles": "sharedfiles",
+            "cddvd": "cddvd",
+            "usbfiles": "usbfiles",
+            "cloudfiles": "cloudfiles",
+            "wsindexer": "wsindexer",
+            "webmsg": "webmsg",
+            "conversations": "conversations",  
+
+        }
+
+        self.HTTP_ERRORS = {
+
+            "304": "Not Modified",
+            "400": "Bad request",
+            "401": "Not authorized",
+            "403": "Fordidden",
+            "501": "Not implemented",
+            "503": "Service unavailable", 
+
+        }   #   Just FYI
+
+        self.ST_INT_API_ERRORS = {
+            "5": "NotAuthenticated. There is no header with bearer token or Basic not sucsessful.",
+            "7": "AccessForbidden. There is no appropriate right on your token.",
+            "11": "Invalid upload rule. Database not exits OR rotation group not exists.",
+            "13": "UserTokenInvalid. Token type is not supported.",
+            "14": "UserTokenExpired. Token has been expired.",
+            "15": "UserTokenInvalidSignature. Token digital signature is not valid.",
+        }
+    
+    def error_handler(self, http_error_code, int_error_code):
+        # pass    #   TODO:   make API HTTP error codes and internal error codes handler
+
+        if http_error_code == 401 and int_error_code == 5:
+            print(self.ST_INT_API_ERRORS("5"))
+        elif http_error_code == 401 and int_error_code == 13:
+            print(self.ST_INT_API_ERRORS("13"))
+        elif http_error_code == 401 and int_error_code == 14:
+            return(self.ST_INT_API_ERRORS("14"))
+        elif http_error_code == 401 and int_error_code == 15:
+            return(self.ST_INT_API_ERRORS("15"))
+        elif http_error_code == 403 and int_error_code == 7:
+            return(self.ST_INT_API_ERRORS("7"))
+        #   Otherwise print an error
+        else:
+            if ((http_error_code not in self.HTTP_ERRORS) and (int_error_code not in self.ST_INT_API_ERRORS)):
+                print("Unknown error: ", str(http_error_code), json.loads(int_error_code))
